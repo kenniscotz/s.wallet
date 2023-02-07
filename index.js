9ecb9327a660c42c4b3d88c16dd7e27d7150a0f3
kenniscotz <74797589+kenniscotz@users.noreply.github.com>
2023-02-08 00:49:39 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+// const hamburger = document.getElementsByClassName("fa-solid ")[0]
+// const navMenu = document.getElementsByClassName("click")[0]
+// // const navMenu = document.getElementsByTagName("ul")[0]
+
+//  hamburger.addEventListener("click", () => {
+//      hamburger.classList.toggle("active");
+//      navMenu.classList.toggle("active");
+//  })
+
+// hamburger.onClick=(alert('Page is working'));
